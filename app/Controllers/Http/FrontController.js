8febb79362789d07983aa8aa01c40ac8826146a5
HinNg <hin.ng@tvb.com>
2018-08-04 16:49:11 +0800

    add session into project

--- app/Controllers/Http/FrontController.js
+++ app/Controllers/Http/FrontController.js
@@ -1,18 +1,23 @@
 'use strict'
 
 class FrontController {
-    async index({view}){
-
+    async index({session, view, response}){
+        var is_login = session.get('is_logged_in')
+        console.log(is_login)
+        if(is_login){
+            response.redirect('/front/student/listing')
+        }
         return view.render('home')
     }
-    async studentlist({view}){
+    async studentlist({session, view}){
         return view.render('student_listing')
     }
 
-    async Login({request, view}){
+    async Login({session, request, view}){
         console.log(request.all())
         var {username, password} = request.all(['username', 'password'])
         if(username == 'admin'&& password== 'admin'){
+            session.put('is_logged_in', true)
             return view.render('student_listing')
         } else {
             return view.render('home')
