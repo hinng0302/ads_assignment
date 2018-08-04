1778bb605f2f967c49daea7fb629f5f65600ba1d
HinNg <hin.ng@tvb.com>
2018-08-04 13:53:30 +0800

    remove button script

--- resources/views/script/student_listing_datatable.js
+++ resources/views/script/student_listing_datatable.js
@@ -1,8 +1,8 @@
 // $(document).ready(function(){
 var table = $("#student_listing").DataTable({
-    // "processing": true,
-    // "serverSide": true,
-    "ajax": "/student/",
+    "processing": true,
+    "serverSide": true,
+    "ajax": "http://223.17.69.134/student",
     "columns": [
         {data:'studentID'},
         {data:'student_name'},
