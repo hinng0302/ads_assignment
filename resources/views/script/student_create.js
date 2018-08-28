02a608f2eab3121cdff2df1751c0d20fa8b956b9
HinNg <hin.ng@tvb.com>
2018-08-28 23:36:55 +0800

    add validation

--- resources/views/script/student_create.js
+++ resources/views/script/student_create.js
@@ -3,23 +3,31 @@ $(document).ready(function(){
         var StudentId = $("#StudentId").val()
         var StudentName = $("#StudentName").val()
         var DOB = $("#DOB").val()
-        $.ajax({
-            url: '/student',
-            type: 'PUT',
-            data: JSON.stringify({
-                "studentID": StudentId,
-                "student_name": StudentName,
-                "DoB": DOB
-            }),
-            headers: {
-                "Content-Type": "application/json",
-                "accept": 'application/json'
-            },
-            success: function(data){
-                if(data.success == true){
-                    window.location = "/front/student/listing"
+        if(StudentID == ''){
+            alert('StudentID Cannot Empty')
+        }else if(StudentName == ''){
+            alert('Student Name Cannot Empty')
+        } else if(DOB == ''){
+            alert('Date of Birth Cannot Empty')
+        }else {
+            $.ajax({
+                url: '/student',
+                type: 'PUT',
+                data: JSON.stringify({
+                    "studentID": StudentId,
+                    "student_name": StudentName,
+                    "DoB": DOB
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "accept": 'application/json'
+                },
+                success: function(data){
+                    if(data.success == true){
+                        window.location = "/front/student/listing"
+                    }
                 }
-            }
-        })
+            })
+        }
     })
 })
