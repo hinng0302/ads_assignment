fff295f72ae4b9d22079abc445d8847665042f52
HinNg <hin.ng@tvb.com>
2018-08-28 11:55:58 +0800

    add offer url

--- app/Controllers/Http/OfferController.js
+++ app/Controllers/Http/OfferController.js
@@ -7,7 +7,15 @@ class OfferController {
         const {Dept_id, CourseID, Year, ClassSize} = request.all(['Dept_id', 'CourseID', 'Year', 'ClassSize'])
         var ret = {}
         try{
-            var offer = new Offer(request.all(['Dept_id', 'CourseID', 'Year', 'ClassSize']))
+            var object = {
+                Dept_id: Dept_id,
+                CourseID: CourseID,
+                Year: Year,
+                ClassSize: ClassSize,
+                AvaliablePlaces: AvaliablePlaces
+            }
+
+            var offer = new Offer(object)
             await offer.save()
             await got(Env.get('DEFAULT_DOMAIN')+'/upate/course/'+CourseID)
             await got(Env.get('DEFAULT_DOMAIN')+'/upate/dept/'+Dept_id)
@@ -23,6 +31,10 @@ class OfferController {
     }
     async get({response}){
         var offer= await Offer.fetch()
+        response.json(offer)
+    }
+    async delete({params, response}){
+        var offer = await Offer.find(params._id).delete()
         response.json(offer)
     }
 }
