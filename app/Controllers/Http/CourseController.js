2e935364b08d6bcad91e4d5412da58b6eabdf4ca
HinNg <hin.ng@tvb.com>
2018-09-01 08:59:39 +0800

    add sort

--- app/Controllers/Http/CourseController.js
+++ app/Controllers/Http/CourseController.js
@@ -135,6 +135,7 @@ class CourseController {
         // const EmbedCourse = use('App/Models/EmbedCourse')
         // var embercourse = await EmbedCourse.fetch()
         // .count({'$size':'enrolled'})
+        const Database = use('Database')
         const mongoClient = await Database.connect()
         const result = await mongoClient.collection('EmbedCourse').find( {enrolled: []} ).fetch()
 
