662b6a0fb8c8c2fa6c5c9a7b1e1993279a446f53
HinNg <hin.ng@tvb.com>
2018-09-01 22:33:20 +0800

    remove trash function from ui

--- resources/views/script/course_listing_datatable.js
+++ resources/views/script/course_listing_datatable.js
@@ -3,7 +3,12 @@ var table = $("#student_listing").DataTable({
     "searching": false,
     "columns": [
         {data:'CourseID'},
-        {data:'Title'},
+        {
+            data:'Title',
+            render: function(data, type, full, meta){
+                return '<a href="/front/course/details/'+full.CourseID+'">'+full.Title+'</a>'
+            }
+        },
         {data:'Level'},
         {data:'created_at'},
         {data:'updated_at'},
@@ -15,31 +20,31 @@ var table = $("#student_listing").DataTable({
     ],
 })
 
-$("#student_listing").on('click', 'i.far.fa-edit', function(){
-    var data = table.row( $(this).parents('tr') ).data()
-    console.log(data)
-    var CourseID = data['CourseID']
-    window.location = '/front/course/edit/'+CourseID
-})
+// $("#student_listing").on('click', 'i.far.fa-edit', function(){
+//     var data = table.row( $(this).parents('tr') ).data()
+//     console.log(data)
+//     var CourseID = data['CourseID']
+//     window.location = '/front/course/edit/'+CourseID
+// })
 
-$("#student_listing").on('click', 'i.fa-trash-alt', function(){
-    var data = table.row( $(this).parents('tr') ).data()
-    var CourseID = data['CourseID']
-    $.ajax({
-        url: '/course',
-        type: 'DELETE',
-        data: JSON.stringify({
-            "CourseID": CourseID
-        }),
-        headers: {
-            "Content-Type": "application/json",
-            "accept": 'application/json'
-        },
-        success: function(data){
-            window.location = "/front/course/listing"
-        }
-    })
-})
+// $("#student_listing").on('click', 'i.fa-trash-alt', function(){
+//     var data = table.row( $(this).parents('tr') ).data()
+//     var CourseID = data['CourseID']
+//     $.ajax({
+//         url: '/course',
+//         type: 'DELETE',
+//         data: JSON.stringify({
+//             "CourseID": CourseID
+//         }),
+//         headers: {
+//             "Content-Type": "application/json",
+//             "accept": 'application/json'
+//         },
+//         success: function(data){
+//             window.location = "/front/course/listing"
+//         }
+//     })
+// })
 
 $("#student_listing tbody").on('click', 'tr', function(){
     var row = table.row( this ).data()
