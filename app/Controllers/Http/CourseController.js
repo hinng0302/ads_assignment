75cc24829c7849ae1b0634455c36e6eed791c72d
HinNg <hin.ng@tvb.com>
2018-09-01 09:02:55 +0800

    add sort

--- app/Controllers/Http/CourseController.js
+++ app/Controllers/Http/CourseController.js
@@ -137,7 +137,7 @@ class CourseController {
         // .count({'$size':'enrolled'})
         const Database = use('Database')
         const mongoClient = await Database.connect()
-        const result = await mongoClient.collection('embed_courses').fetch()
+        const result = await mongoClient.collection('embed_courses')// .fetch()
 
         response.json({data: result})
     }
